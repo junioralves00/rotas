1adf48953d0409b08bb8c34734691f2b7fff1bc2
junioralves00 <junioralves00@gmail.com>
2020-07-22 08:45:40 -0300

    Aula 55

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { CursosService } from './cursos.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
@@ -9,12 +12,36 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
    cursos: any[];
+   pagina:number;
+   inscricao: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+      ) { }
 
   ngOnInit(): void {
     
     this.cursos = this.cursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any)=> {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    //this.pagina++;
+    this.router.navigate(['/cursos'],
+        {queryParams: {'pagina': this.pagina++}});
   }
 
 
